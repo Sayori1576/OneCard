bbb2a5eef875d68086edcb432842c75b6c0f3194
Sayori1576 <arwen02@naver.com>
2022-01-29 10:22:57 +0000

    inputoutbase들 초안 작성

---
+++ include/glInoutputbase.h
@@ -0,0 +1,10 @@
+#include "Default.h"
+class Glinoutputbase
+{
+public:
+virtual bool quitment()=0;
+virtual void startment()=0;
+virtual int inputplayernum()=0;
+virtual std::string inputplayername()=0;
+
+};
