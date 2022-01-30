05d6fc6d8fbebd2a6bdd8a8bb7a85a726e247990
Sayori1576 <arwen02@naver.com>
2022-01-30 13:11:51 +0000

    컴파일 오류 수정

---
+++ include/plinoutputbase.h
@@ -0,0 +1,12 @@
+class plinoutputbase
+{
+virtual void printinfo();
+virtual void attackment();
+virtual void defenctment();
+virtual void onemorement();
+virtual void inputcardkind();
+virtual void changekindment();
+virtual void printandinputcard();
+virtual void cardmeokgiment();
+virtual void princardsize();
+};
